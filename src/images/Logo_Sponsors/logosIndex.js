0f365a6d95c29557b563d59af9d0878e5ec68a8a
Fede <fredez1991@gmail.com>
2025-12-11 13:49:40 +0100

    adding one sponsor and changing styles

--- src/images/Logo_Sponsors/logosIndex.js
+++ src/images/Logo_Sponsors/logosIndex.js
@@ -3,11 +3,13 @@ import sponsor2 from './Logo_Swisslos_black.png'
 import sponsor3 from './Logo_MEVO_black.png'
 import sponsor4 from './Logo_Schibler_black.png'
 import sponsor5 from './Logo_Joma_black.png'
+import sponsor6 from './Logo_JohnBaker_black.png'
 
 export const sponsors = {
   sponsor1,
   sponsor2,
   sponsor3,
   sponsor4,
-  sponsor5
+  sponsor5,
+  sponsor6
 }
